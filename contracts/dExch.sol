9afd279e1a7041bdab0a6ff8e04256b5b2cc40f6
HenricoW <henrico.ashley.White@gmail.com>
2021-02-23 21:47:21 +0200

    added user deposit & withdraw, minor data structure changes

--- contracts/dExch.sol
+++ contracts/dExch.sol
@@ -1,19 +1,23 @@
 pragma solidity ^0.6.3;
 
+// import IERC20 from OpenZeppelin
+import './IERC20.sol';
+
 /// @title Contract for ERC-20 based decentralised exchange
 contract dExch {
 
     // token properties
-    struct Token {
-        bytes32 ticker;
-        address tokenAddr;
-    }
+    struct Token { bytes32 ticker; address tokenAddr; }
     address public admin;
-    Token[] public tokenList
+    bytes32[] public tokenList
     mapping (bytes32 => Token) public tokenDetails;
 
     // user properties
-    mapping(bytes32 => mapping(address => uint)) public userBalances;
+    mapping(address => mapping(bytes32 => uint)) public userBalances;
+
+    // order properties
+    // order struct {id, creator, token, quoteToken, amount, price, date, remaining, filled}
+    // array of orders
 
     // modifiers
     modifier onlyAdmin() {
@@ -21,13 +25,38 @@ contract dExch {
         _;
     }
 
-    constructor () public {
+    modifier tokenExists(bytes32 ticker) {
+        require(tokenDetails[ticker].address != address(0), "Token not listed");
+        _;
+    }
+
+    constructor() public {
         admin = msg.sender;
     }
 
-    function addToken(bytes32 _ticker, address _tokenAddr) public onlyAdmin() {
-        Token memory tkn = Token(_ticker, _tokenAddr);
-        tokenList.push(tkn);
-        tokenDetails[_ticker] = tkn;
+    // add token - only admin
+    function addToken(bytes32 _ticker, address _tokenAddr) external onlyAdmin() {
+        tokenDetails[_ticker] = Token(_ticker, _tokenAddr);
+        tokenList.push(_ticker);
     }
+
+    // user: deposit (ticker, amount) - use transferFrom(user, self, amount) -> usr will have to apporve contract, IERC20.sol on ticker addr
+    function deposit(bytes32 ticker, uint amount) external tokenExists(ticker) {
+        IERC20 _tkn = IERC20(tokenDetails[ticker].tokenAddr)
+        // require(_tkn.balanceOf(msg.sender) >= amount, 'Insufficient tokan balance');
+        userBalances[msg.sender][ticker] += amount;
+
+        _tkn.transferFrom(msg.sender, address(this), amount);
+    }
+
+    // user: withdraw (ticker, amount) - only if suffic balance - use transfer fn, IERC20.sol on ticker addr
+    function withdraw(bytes32 ticker, uint amount) external tokenExists(ticker) {
+        require(userBalances[msg.sender][ticker] >= amount, 'Insufficient token balance')
+        IERC20 _tkn = IERC20(tokenDetails[ticker].tokenAddr)
+        userBalances[msg.sender][ticker] -= amount;
+
+        _tkn.transfer(msg.sender, amount);
+    }
+
+    // user: createLimitOrder(token, amount, price)
 }
