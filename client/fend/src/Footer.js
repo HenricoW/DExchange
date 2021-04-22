330069784d4eff9f7afff47cc7e5d3c655e4c684
HenricoW <henrico.ashley.White@gmail.com>
2021-04-22 22:23:51 +0200

    modified footer

--- client/fend/src/Footer.js
+++ client/fend/src/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <p className="text-center">
-  From  <a href="http://pro.eattheblocks.com" style={{color: 'white'}}>6-Figures Blockchain dev</a>, a Blockchain course to become a Profesional Blockchain developer
+        <a href="#" style={{color: 'white'}}>Trading Dex</a> - Copyright 2021
       </p>
     </footer>
   );
