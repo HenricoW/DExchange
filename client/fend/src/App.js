8a1295e2c7ac67f4ad21d41aaaf9d2e6dfb1380a
HenricoW <henrico.ashley.White@gmail.com>
2021-04-27 21:35:49 +0200

    added all orders and all trades components

--- client/fend/src/App.js
+++ client/fend/src/App.js
@@ -1,8 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Footer from './Footer.js';
 import Header from "./Header";
 import Wallet from "./Wallet.js";
 import NewOrder from "./NewOrder";
+import AllOrders from "./AllOrders";
+import AllTrades from "./AllTrades";
+
+const SIDE = {
+  BUY: 0,
+  SELL: 1
+}
 
 function App({ web3, contracts, accounts }) {
   const [tokens, setTokens] = useState(undefined);
@@ -13,14 +20,24 @@ function App({ web3, contracts, accounts }) {
   const [balances, setBalances] = useState({
     tokenDex: 0,
     tokenWallet: 0
-  })
+  });
+  const [orders, setOrders] = useState({
+    buy: [],
+    sell: []
+  });
+  const [trades, _setTrades] = useState([]);
+  const [listener, setListener] = useState(undefined);
+
+  const tradesRef = useRef(trades);
   
   useEffect(() => {
     const init = async () => {
       const rawTokens = await contracts.dex.methods.getTokens().call();
       const dexBal = await contracts.dex.methods.userBalances(accounts[0], rawTokens[0].ticker).call();
       const tickerText = web3.utils.hexToUtf8(rawTokens[0].ticker);
-      const wallBal = await contracts[tickerText].methods.balanceOf(accounts[0]).call()
+      const wallBal = await contracts[tickerText].methods.balanceOf(accounts[0]).call();
+      await getOrders(rawTokens[0]);
+      setTrades([]);
   
       setTokens(rawTokens);
       setUser({
@@ -31,12 +48,65 @@ function App({ web3, contracts, accounts }) {
         tokenDex: dexBal,
         tokenWallet: wallBal
       });
-      console.log("useEffect: ", user);
     }
 
     init();
 
   }, []);
+
+  useEffect(() => {
+    const init = async () => {
+      setTrades([]);
+      if(user.selectedToken === undefined) return
+      getTrades(user.selectedToken);
+    }
+
+    init();
+  }, [user.selectedToken],
+   () => {
+      listener.unsubscribe();                                       // when exactly will this be fired? Why not before new getTrades() call?
+    }
+  );
+
+  const setTrades = trade => {
+    let nuTrades = tradesRef.current;
+
+    if(trade.length === 0){
+      tradesRef.current = [];
+      _setTrades([]);
+    } else {
+      nuTrades.push(trade);
+      tradesRef.current = nuTrades;
+      _setTrades(nuTrades);
+    }
+  }
+
+  const getTrades = async theToken => {
+    let tradeIds = new Set();
+    console.log("getTrades fired");
+    const _listener = contracts.dex.events.tradeExecuted({
+      filter: { ticker: theToken.ticker },                        // filter only works on indexed fields in the event !!
+      fromBlock: 0                                                // INEFFICIENT for production OR testnet !!!!
+    })
+    .on('data', data => {
+      if(tradeIds.has(data.returnValues.tradeId)) { 
+        return
+      } else {
+        tradeIds.add(data.returnValues.tradeId);
+        setTrades(data.returnValues);
+      }
+    });
+
+    setListener(_listener);
+  }
+
+  const getOrders = async (token) => {
+    const orderLists = await Promise.all([
+      contracts.dex.methods.viewOrderBook(token.ticker, SIDE.BUY).call(),
+      contracts.dex.methods.viewOrderBook(token.ticker, SIDE.SELL).call()
+    ]);
+    setOrders({buy: orderLists[0], sell: orderLists[1]});
+  }
 
   const onSelect = async item => {
     setUser({
@@ -44,13 +114,13 @@ function App({ web3, contracts, accounts }) {
       selectedToken: item.value
     });
     await updateBalances(user.accounts[0], item.value);
+    await getOrders(item.value);
   }
 
   const updateBalances = async (account, token) => {
     let dexBal = await contracts.dex.methods.userBalances(account, token.ticker).call();
     const tickerText = web3.utils.hexToUtf8(token.ticker);
     let wallBal = await contracts[tickerText].methods.balanceOf(account).call();
-    console.log(web3.utils.hexToUtf8(user.selectedToken.ticker));
     setBalances({ tokenDex: dexBal, tokenWallet: wallBal });
   }
 
@@ -62,17 +132,18 @@ function App({ web3, contracts, accounts }) {
   }
   
   const withdraw = async amount => {
-    const tickerText = web3.utils.hexToUtf8(user.selectedToken.ticker);
     await contracts.dex.methods.withdraw(user.selectedToken.ticker, amount).send({from: user.accounts[0]});
     await updateBalances(user.accounts[0], user.selectedToken);
   }
 
   const createMarketOrder = async (side, amount) => {
     await contracts.dex.methods.createMarketOrder(user.selectedToken.ticker, side, amount).send({from: accounts[0]});
+    await getOrders(user.selectedToken);
   }
 
   const createLimitOrder = async (side, amount, price) => {
     await contracts.dex.methods.createLimitOrder(side, user.selectedToken.ticker, amount, price).send({from: accounts[0]});
+    await getOrders(user.selectedToken);
   }
 
   if(tokens === undefined || user.selectedToken === undefined || contracts === undefined) return <div>Loading...</div>
@@ -92,6 +163,10 @@ function App({ web3, contracts, accounts }) {
                 createMarketOrder={createMarketOrder}
               />
             }
+          </div>
+          <div className="col-sm-8">
+            <AllOrders orders={orders}/>
+            <AllTrades trades={tradesRef.current} />
           </div>
         </div>
       </main>
