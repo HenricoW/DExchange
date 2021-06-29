04e736beaaea67eba7900f8b24d8a579c124e30a
HenricoW <henrico.ashley.White@gmail.com>
2021-06-29 14:04:09 +0200

    app name, display client acc address in header

--- client/fend/src/Footer.js
+++ client/fend/src/Footer.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-  return (
-    <footer>
-      <p className="text-center">
-        <a href="#" style={{color: 'white'}}>Trading Dex</a> - Copyright 2021
-      </p>
-    </footer>
-  );
+    return (
+        <footer>
+            <p className="text-center">
+                <a href="#" style={{ color: "white" }}>
+                    Ash Exchange
+                </a>{" "}
+                - Copyright 2021
+            </p>
+        </footer>
+    );
 };
 
 export default Footer;
--- client/fend/src/Header.js
+++ client/fend/src/Header.js
@@ -1,32 +1,35 @@
-import Dropdown from './Dropdown'
-import React from 'react'
+import Dropdown from "./Dropdown";
+import React from "react";
 
 const Header = ({ dex, user, tokens, onSelect, web3 }) => {
     return (
         <header id="header" className="card">
             <div className="row">
                 <div className="col-sm-2 flex">
-                    <Dropdown 
-                        items={tokens.map(item => ({
+                    <Dropdown
+                        items={tokens.map((item) => ({
                             label: web3.utils.hexToUtf8(item.ticker),
-                            value: item
+                            value: item,
                         }))}
-
                         activeItem={{
                             label: web3.utils.hexToUtf8(user.selectedToken.ticker),
-                            value: user.selectedToken
+                            value: user.selectedToken,
                         }}
-
                         onSelect={onSelect}
                     />
-                </div> 
+                </div>
                 <div className="col-sm-10s">
-                    <h4>Ash Exchange</h4>
-                    <h6><span className="contract-addr">Address: <span className="address">{dex.options.address}</span></span> </h6>
+                    <h2>Ash Exchange v0.1</h2>
+                    <h6>
+                        <span className="contract-addr">
+                            Contract address: <span className="address">{dex.options.address}</span>
+                        </span>{" "}
+                    </h6>
+                    <h6>Connected account: {user.accounts[0]}</h6>
                 </div>
             </div>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
