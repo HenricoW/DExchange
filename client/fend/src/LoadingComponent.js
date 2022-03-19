23889be2e4cfbd0e71cf59150c42f1484d002a18
HenricoW <henrico.ashley.White@gmail.com>
2022-03-19 12:32:05 +0200

    fix: new MM api, events & error handling

--- client/fend/src/LoadingComponent.js
+++ client/fend/src/LoadingComponent.js
@@ -3,32 +3,43 @@ import { getWeb3, getContracts } from "./utils";
 import App from "./App";
 
 const LoadingComponent = () => {
-    const [web3, setWeb3] = useState(undefined);
-    const [contracts, setContracts] = useState(undefined);
-    const [accounts, setAccounts] = useState(undefined);
+  const [web3, setWeb3] = useState(undefined);
+  const [contracts, setContracts] = useState(undefined);
+  const [accounts, setAccounts] = useState(undefined);
+  const [chainId, setChainId] = useState(0);
 
-    useEffect(() => {
-        const init = async () => {
-            const WEB3 = await getWeb3();
-            const CONTR = await getContracts(WEB3);
-            const ACCS = await WEB3.eth.getAccounts();
-            setWeb3(WEB3);
-            setContracts(CONTR);
-            setAccounts(ACCS);
+  const connectWeb3 = async () => {
+    const WEB3 = await getWeb3();
+    setWeb3(WEB3);
+    const ACCS = await window.ethereum.request({ method: "eth_requestAccounts" });
+    setAccounts(ACCS);
 
-            window.ethereum.on("accountsChanged", (accs) => handleAccChg(accs));
-        };
+    const chainid = await WEB3.eth.getChainId();
+    setChainId(chainid);
+    if (chainid !== 4) return;
 
-        init();
-    }, []);
+    const CONTR = await getContracts(WEB3);
+    setContracts(CONTR);
 
-    const handleAccChg = (accs) => (accs.length === 0 ? console.log("Please connect to MetaMask") : setAccounts(accs));
+    window.ethereum.on("accountsChanged", (accs) => handleAccChg(accs));
+    window.ethereum.on("chainChanged", (_chainId) => window.location.reload());
+  };
 
-    return web3 === undefined || contracts === undefined || accounts === undefined ? (
-        <h3>Loading.......</h3>
-    ) : (
-        <App web3={web3} contracts={contracts} accounts={accounts} />
-    );
+  const handleAccChg = (accs) => (accs.length === 0 ? window.location.reload() : setAccounts(accs));
+
+  return web3 === undefined || contracts === undefined || accounts === undefined || chainId !== 4 ? (
+    <div className="text-center pt-5 mt-5">
+      <h3 className="mt-5">Please switch your MetaMask to the Rinkeby testnet and click CONNECT</h3>
+      <h3 className="mt-4">
+        PLEASE NOTE: This site is best viewed on a desk/laptop and not suited for smaller screens
+      </h3>
+      <button type="submit" className="btn btn-primary mt-5" onClick={async () => await connectWeb3()}>
+        Connect MetaMask
+      </button>
+    </div>
+  ) : chainId === 4 ? (
+    <App web3={web3} contracts={contracts} accounts={accounts} />
+  ) : null;
 };
 
 export default LoadingComponent;
