f52dba6bcccbd94c9164cebcf52b72f9d59866fe
HenricoW <henrico.ashley.White@gmail.com>
2022-03-19 17:26:09 +0200

    styling & layout fixes

--- client/fend/src/Dropdown.js
+++ client/fend/src/Dropdown.js
@@ -1,34 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Dropdown = ({items, activeItem, onSelect}) => {
+const Dropdown = ({ items, activeItem, onSelect }) => {
+  const [dropdownVisible, setDropDownVisible] = useState(false);
 
-    const [dropdownVisible, setDropDownVisible] = useState(false);
+  const selectItem = async (e, item) => {
+    e.preventDefault();
+    setDropDownVisible(!dropdownVisible);
+    await onSelect(item);
+  };
 
-    const selectItem = async (e, item) => { 
-        e.preventDefault();
-        setDropDownVisible(!dropdownVisible);
-        await onSelect(item);
-    }
-
-    return (
-        <div className="dropdown ml-3">
-            <button className="btn btn-secondary dropdown-toggle" type="button" onClick={() => setDropDownVisible(!dropdownVisible)} >
-                {activeItem.label}
-            </button>
-            <div className={`dropdown-menu ${dropdownVisible ? "visible" : ""}`}>
-                {items && items.map((item, idx) => (
-                    <a 
-                        className={`dropdown-item ${item.label === activeItem.label ? "active" : ""}`} 
-                        href="#" 
-                        key={idx}
-                        onClick={async e => await selectItem(e, item)}
-                        >
-                            {item.label}
-                    </a>
-                ))}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="dropdown ml-3">
+      <button
+        className="btn btn-secondary dropdown-toggle"
+        type="button"
+        onClick={() => setDropDownVisible(!dropdownVisible)}
+        style={{ fontSize: "20px" }}
+      >
+        {activeItem.label}
+      </button>
+      <div className={`dropdown-menu ${dropdownVisible ? "visible" : ""}`}>
+        {items &&
+          items.map((item, idx) => (
+            <a
+              className={`dropdown-item ${item.label === activeItem.label ? "active" : ""}`}
+              href="#"
+              key={idx}
+              onClick={async (e) => await selectItem(e, item)}
+            >
+              {item.label}
+            </a>
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default Dropdown;
--- client/fend/src/Header.js
+++ client/fend/src/Header.js
@@ -2,34 +2,37 @@ import Dropdown from "./Dropdown";
 import React from "react";
 
 const Header = ({ dex, user, tokens, onSelect, web3 }) => {
-    return (
-        <header id="header" className="card">
-            <div className="row">
-                <div className="col-sm-2 flex">
-                    <Dropdown
-                        items={tokens.map((item) => ({
-                            label: web3.utils.hexToUtf8(item.ticker),
-                            value: item,
-                        }))}
-                        activeItem={{
-                            label: web3.utils.hexToUtf8(user.selectedToken.ticker),
-                            value: user.selectedToken,
-                        }}
-                        onSelect={onSelect}
-                    />
-                </div>
-                <div className="col-sm-10s">
-                    <h2>Ash Exchange v0.3</h2>
-                    <h6>
-                        <span className="contract-addr">
-                            Contract address: <span className="address">{dex.options.address}</span>
-                        </span>{" "}
-                    </h6>
-                    <h6>Connected account: {user.accounts[0]}</h6>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header id="header" className="card">
+      <div className="row px-5 py-2 d-flex justify-content-between">
+        <div className="d-flex">
+          <h2 className="mr-5">OB DEX v0.3</h2>
+          <div className="col-sm-10s">
+            <h6>
+              <span className="contract-addr">
+                Contract address: <span className="address">{dex.options.address}</span>
+              </span>{" "}
+            </h6>
+            <h6>Connected account: {user.accounts[0]}</h6>
+          </div>
+        </div>
+        <div className="col-sm-2 flex align-items-center" style={{ minWidth: "400px" }}>
+          <h4>Current pair: {web3.utils.hexToUtf8(user.selectedToken.ticker)}-DAI</h4>
+          <Dropdown
+            items={tokens.map((item) => ({
+              label: web3.utils.hexToUtf8(item.ticker),
+              value: item,
+            }))}
+            activeItem={{
+              label: web3.utils.hexToUtf8(user.selectedToken.ticker),
+              value: user.selectedToken,
+            }}
+            onSelect={onSelect}
+          />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
